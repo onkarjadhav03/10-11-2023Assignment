167e5603c9a97c842a6b187e79584396abf1ad58
onkar <jadhavonkar333@gmail.com>
2023-11-10 15:33:25 +0530

    Assignment6

---
+++ Assignment6_P1/Assignment6_P1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6_P1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] matrix = new int[3][];
+            int element;
+
+            matrix[0] = new int[4] { 1, 2, 3, 4 };
+            matrix[1] = new int[2] { 1, 2 };
+            matrix[2] = new int[3] { 1, 2, 3 };
+
+            Console.WriteLine("Stored values are:");
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                Console.WriteLine("Internal elements " + (i + 1));
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    Console.WriteLine(matrix[i][j] + " ");
+                }
+                //element = matrix[0][1];
+
+
+            }
+            Console.WriteLine("\nSecond element of the first row: " + matrix[0][1]);
+            // Add a new third element to the second row
+            int newElement = 15;
+            Array.Resize(ref matrix[1], matrix[1].Length + 1);
+            matrix[1][2] = newElement;
+
+            // Print the updated matrix
+            Console.WriteLine("\nUpdated Matrix:");
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                Console.WriteLine("Internal elements " + (i + 1));
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    Console.Write(matrix[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+
+            // Create a dynamic jagged array with predefined values
+            //object[][] dynamicArray = new object[][]
+            //{
+            //     new object[] { 1, "Hello", 3.14 },
+            //     new object[] { true, 'A', DateTime.Now },
+
+            //};
+
+            //// Access and print the elements of the array
+            //for (int i = 0; i < dynamicArray.Length; i++)
+            //{
+            //    Console.WriteLine("Row " + (i + 1) + ":");
+            //    for (int j = 0; j < dynamicArray[i].Length; j++)
+            //    {
+            //        Console.Write(dynamicArray[i][j] + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Assignment6_P2/Assignment6_P2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Assignment6_P2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList studentGrade = new ArrayList()
+            {
+                85,92,78,95,88
+            };
+
+            int sum = 0;
+
+            Console.WriteLine("Student Grades");
+            foreach (int i in studentGrade)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+            Console.WriteLine("sum of grades:" + sum);
+
+            studentGrade.Add(90);
+
+            Console.WriteLine("StudentGrades after adding new grade");
+            foreach (int i in studentGrade)
+            {
+                Console.WriteLine(i);
+            }
+
+            studentGrade.Remove(78);
+            Console.WriteLine("StudentGrades after deleting");
+            foreach (int i in studentGrade)
+            {
+                Console.WriteLine(i);
+            }
+
+            if (studentGrade.Contains(95))
+            {
+                int update=studentGrade.IndexOf(95);
+                studentGrade[update] = 96;
+                Console.WriteLine("StudentGrades after updation");
+                foreach (int i in studentGrade)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("grade 95 not found");
+            }
+        }
+    }
+}
